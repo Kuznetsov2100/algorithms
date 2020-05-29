7b0fd481018d437f5d3943986b4fba606c5ed2b4
handane123 <7983700+handane123@users.noreply.github.com>
2020-05-29 21:48:05 +0800

    add mergesort

--- sort/sort.go
+++ sort/sort.go
@@ -1,5 +1,21 @@
 package sort
 
+func BubbleSort(data Comparable) {
+	n := data.Len()
+	for i := 0; i < n-1; i++ {
+		isChanged := false
+		for j := 0; j < n-1-i; j++ {
+			if data.Less(j+1, j) {
+				data.Swap(j+1, j)
+				isChanged = true
+			}
+		}
+		if !isChanged {
+			break
+		}
+	}
+}
+
 // InsertionSort sorts Comparable type using insertion sort.
 // In the worst case, this implementation makes ~ 1/2*n^2 compares
 // and ~ 1/2*n^2 exchanges to sort an array of length n.
@@ -39,12 +55,55 @@ func ShellSort(data Comparable) {
 	for gap := n / 2; gap > 0; gap = gap / 2 {
 		for i := gap; i < n; i++ {
 			j := i
-			for j-gap >= 0 && data.Less(j, j-gap) {
+			for ; j-gap >= 0 && data.Less(j, j-gap); j = j - gap {
 				data.Swap(j, j-gap)
-				j = j - gap
 			}
 		}
 	}
+}
+
+// MergeSort sorts Comparable type using a top-down, recursive version of mergesort.
+// This implementation takes O(n*logn) time to sort any array of length n (assuming comparisons take constant time).
+// It makes between ~ 1/2*N*log2N and ~ 1*N*log2N compares.
+// This sorting algorithm is stable. It uses O(N) extra memory (not including the input array).
+func MergeSort(data Comparable) {
+	aux := make([]int, data.Len())
+	var sort func(data Comparable, low, high int)
+	merge := func(data Comparable, low, mid, high int) {
+		li, ls := low, mid
+		ri, rs := mid, high
+		cursor := 0
+		for ; li < ls && ri < rs; cursor++ {
+			if data.Less(li, ri) {
+				aux[li-low] = cursor
+				li++
+			} else {
+				aux[ri-low] = cursor
+				ri++
+			}
+		}
+		for ; li < ls; li++ {
+			aux[li-low] = cursor
+			cursor++
+		}
+		for i := range aux[:cursor] {
+			for j := aux[i]; j != i; {
+				data.Swap(low+i, low+j)
+				aux[i], aux[j], j = aux[j], aux[i], aux[j]
+			}
+		}
+	}
+	sort = func(data Comparable, low, high int) {
+		size := high - low
+		if size < 2 {
+			return
+		}
+		mid := (low + high) / 2
+		sort(data, low, mid)
+		sort(data, mid, high)
+		merge(data, low, mid, high)
+	}
+	sort(data, 0, data.Len())
 }
 
 // IsSorted reports whether data is sorted.
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -41,6 +41,30 @@ func (p IntSlice) Swap(i, j int) {
 	p[i], p[j] = p[j], p[i]
 }
 
+func Test_BubbleSort(t *testing.T) {
+	s := class{
+		{"alan", 95},
+		{"hikerell", 91},
+		{"acmfly", 96},
+		{"leao", 90},
+	}
+
+	if IsSorted(s) {
+		t.Errorf("expect false, got %t", IsSorted(s))
+	}
+	BubbleSort(s)
+	if !IsSorted(s) {
+		t.Errorf("expect true, got %t", IsSorted(s))
+	}
+
+	// to test ischanged = false
+	numbers := IntSlice{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	BubbleSort(numbers)
+	if !IsSorted(numbers) {
+		t.Errorf("expect true, got %t", IsSorted(numbers))
+	}
+}
+
 func Test_InsertionSort(t *testing.T) {
 	s := class{
 		{"alan", 95},
@@ -95,6 +119,35 @@ func Test_ShellSort(t *testing.T) {
 
 }
 
+func Test_MergeSort(t *testing.T) {
+	s := class{
+		{"alan", 95},
+		{"hikerell", 91},
+		{"acmfly", 96},
+		{"leao", 90},
+	}
+
+	if IsSorted(s) {
+		t.Errorf("expect false, got %t", IsSorted(s))
+	}
+	MergeSort(s)
+	if !IsSorted(s) {
+		t.Errorf("expect true, got %t", IsSorted(s))
+	}
+
+}
+
+func Benchmark_BubbleSort(b *testing.B) {
+	b.StopTimer()
+	var numbers IntSlice
+	for i := 0; i < 10000; i++ {
+		numbers = append(numbers, rand.Int())
+	}
+	b.StartTimer()
+	BubbleSort(numbers)
+	b.StopTimer()
+}
+
 func Benchmark_InsertionSort(b *testing.B) {
 	b.StopTimer()
 	var numbers IntSlice
@@ -124,5 +177,16 @@ func Benchmark_ShellSort(b *testing.B) {
 	}
 	b.StartTimer()
 	ShellSort(numbers)
+	b.StopTimer()
+}
+
+func Benchmark_MergeSort(b *testing.B) {
+	b.StopTimer()
+	var numbers IntSlice
+	for i := 0; i < 10000; i++ {
+		numbers = append(numbers, rand.Int())
+	}
+	b.StartTimer()
+	MergeSort(numbers)
 	b.StopTimer()
 }
