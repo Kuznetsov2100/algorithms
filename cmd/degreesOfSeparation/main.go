aea5bc4a1e08320fbf922e5e0583fc632d473123
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-18 13:55:18 +0800

    add DegreesOfSeparation

---
+++ cmd/degreesOfSeparation/main.go
@@ -0,0 +1,85 @@
+package main
+
+//   %  go run main.go routes.txt " " "JFK"
+//   LAS
+//      JFK
+//      ORD
+//      DEN
+//      LAS
+//   DFW
+//      JFK
+//      ORD
+//      DFW
+//   EWR
+//      Not in database.
+
+//   % go run main.go movies.txt "/" "Bacon, Kevin"
+//   Kidman, Nicole
+//      Bacon, Kevin
+//      Woodsman, The (2004)
+//      Grier, David Alan
+//      Bewitched (2005)
+//      Kidman, Nicole
+//   Grant, Cary
+//      Bacon, Kevin
+//      Planes, Trains & Automobiles (1987)
+//      Martin, Steve (I)
+//      Dead Men Don't Wear Plaid (1982)
+//      Grant, Cary
+
+//   % go run main.go movies.txt "/" "Animal House (1978)"
+//   Titanic (1997)
+//      Animal House (1978)
+//      Allen, Karen (I)
+//      Raiders of the Lost Ark (1981)
+//      Taylor, Rocky (I)
+//      Titanic (1997)
+//   To Catch a Thief (1955)
+//      Animal House (1978)
+//      Vernon, John (I)
+//      Topaz (1969)
+//      Hitchcock, Alfred (I)
+//      To Catch a Thief (1955)
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/handane123/algorithms/graph"
+	"github.com/handane123/algorithms/stdin"
+)
+
+func main() {
+	filename := os.Args[1]
+	delimiter := os.Args[2]
+	source := os.Args[3]
+
+	sg := graph.NewSymbolGraph(filename, delimiter)
+	G := sg.Graph()
+	if !sg.Contains(source) {
+		fmt.Println(source, " not in database.")
+		return
+	}
+
+	s := sg.IndexOf(source)
+	bfs := graph.NewBreadthFirstPaths(G, s)
+
+	stdin := stdin.NewStdInLine()
+	fmt.Println("Please input a vertex, you will get the path from source to your vertex , use CTRL+D to cancel.")
+	for !stdin.IsEmpty() {
+		sink := strings.Trim(stdin.ReadString(), " ")
+		if sg.Contains(sink) {
+			t := sg.IndexOf(sink)
+			if bfs.HasPathTo(t) {
+				for _, v := range bfs.PathTo(t) {
+					fmt.Println("   ", sg.NameOf(v))
+				}
+			} else {
+				fmt.Println("Not connected.")
+			}
+		} else {
+			fmt.Printf("\"%s\" is not in database.\n", sink)
+		}
+	}
+}
--- cmd/symbolGraph/main.go
+++ cmd/symbolGraph/main.go
@@ -1,5 +1,35 @@
 package main
 
+//   %  go run main.go routes.txt " "
+//   JFK
+//      MCO
+//      ATL
+//      ORD
+//   LAX
+//      PHX
+//      LAS
+
+//   % go run main.go movies.txt "/"
+//   Tin Men (1987)
+//      Hershey, Barbara
+//      Geppi, Cindy
+//      Jones, Kathy (II)
+//      Herr, Marcia
+//      ...
+//      Blumenfeld, Alan
+//      DeBoy, David
+//   Bacon, Kevin
+//      Woodsman, The (2004)
+//      Wild Things (1998)
+//      Where the Truth Lies (2005)
+//      Tremors (1990)
+//      ...
+//      Apollo 13 (1995)
+//      Animal House (1978)
+
+//   Assumes that input file is encoded using UTF-8.
+//   % iconv -f ISO-8859-1 -t UTF-8 movies-iso8859.txt > movies.txt
+
 import (
 	"fmt"
 	"os"
