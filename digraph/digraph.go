7ab370dc64da1e4213c4de173b4df81327736637
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-19 15:31:26 +0800

    add digraph.go with test coverage:100%

--- digraph/digraph.go
+++ digraph/digraph.go
@@ -1,5 +1,21 @@
 package digraph
 
+// A self-loop is an edge that connects a vertex to itself.
+// Two edges are parallel if they connect the same ordered pair of vertices.
+// The outdegree of a vertex is the number of edges pointing from it.
+// The indegree of a vertex is the number of edges pointing to it.
+// A subgraph is a subset of a digraph's edges (and associated vertices) that constitutes a digraph.
+// A directed path in a digraph is a sequence of vertices in which there is a (directed) edge pointing from each vertex in the sequence to its successor in the sequence, with no repeated edges.
+// A directed path is simple if it has no repeated vertices.
+// A directed cycle is a directed path (with at least one edge) whose first and last vertices are the same.
+// A directed cycle is simple if it has no repeated vertices (other than the requisite repetition of the first and last vertices).
+// The length of a path or a cycle is its number of edges.
+// We say that a vertex w is reachable from a vertex v if there exists a directed path from v to w.
+// We say that two vertices v and w are strongly connected if they are mutually reachable: there is a directed path from v to w and a directed path from w to v.
+// A digraph is strongly connected if there is a directed path from every vertex to every other vertex.
+// A digraph that is not strongly connected consists of a set of strongly connected components, which are maximal strongly connected subgraphs.
+// A directed acyclic graph (or DAG) is a digraph with no directed cycles.
+
 import (
 	"fmt"
 	"strings"
@@ -9,6 +25,18 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Digraph struct represents a directed graph of vertices named 0 through V - 1.
+// It supports the following two primary operations: add an edge to the digraph,
+// iterate over all of the vertices adjacent from a given vertex.
+// It also provides methods for returning the indegree or outdegree of a vertex,
+// the number of vertices V in the digraph, the number of edges E in the digraph,
+// and the reverse digraph. Parallel edges and self-loops are permitted.
+// This implementation uses an adjacency-lists representation,
+// which is a vertex-indexed array of Bag objects. It uses O(E + V) space,
+// where E is the number of edges and V is the number of vertices. All instance methods take O(1) time.
+// (Though, iterating over the vertices returned by adj(int) takes time proportional to the
+// outdegree of the vertex.) Constructing an empty digraph with V vertices takes O(V) time;
+// constructing a digraph with E edges and V vertices takes O(E + V) time.
 type Digraph struct {
 	v        int
 	e        int
@@ -16,6 +44,7 @@ type Digraph struct {
 	indegree []int
 }
 
+// NewDigraph initializes an empty graph with V vertices and 0 edges.
 func NewDigraph(V int) *Digraph {
 	if V < 0 {
 		panic("Number of vertices in a Digraph must be non negative")
@@ -33,6 +62,7 @@ func NewDigraph(V int) *Digraph {
 	}
 }
 
+// NewDigraphIn initializes a graph from the specified input stream.
 func NewDigraphIn(in *stdin.In) (*Digraph, error) {
 	if in == nil {
 		return nil, errors.New("argument is nil")
@@ -66,6 +96,7 @@ func NewDigraphIn(in *stdin.In) (*Digraph, error) {
 	return dg, nil
 }
 
+// AddEdge adds the directed edge v-w to this digraph.
 func (dg *Digraph) AddEdge(v, w int) {
 	dg.validateVertex(v)
 	dg.validateVertex(w)
@@ -74,24 +105,29 @@ func (dg *Digraph) AddEdge(v, w int) {
 	dg.e++
 }
 
+// OutDegree returns the number of directed edges incident from vertex v.
 func (dg *Digraph) OutDegree(v int) int {
 	dg.validateVertex(v)
 	return dg.adj[v].Size()
 }
 
+// InDegree returns the number of directed edges incident to vertex v.
 func (dg *Digraph) InDegree(v int) int {
 	dg.validateVertex(v)
 	return dg.indegree[v]
 }
 
+// V returns the number of vertices in this digraph.
 func (dg *Digraph) V() int {
 	return dg.v
 }
 
+// E returns the number of edges in this digraph.
 func (dg *Digraph) E() int {
 	return dg.e
 }
 
+// Adj returns the vertices adjacent to vertex v.
 func (dg *Digraph) Adj(v int) (vertices []int) {
 	for _, x := range dg.adj[v].Values() {
 		vertices = append(vertices, x.(int))
@@ -99,6 +135,7 @@ func (dg *Digraph) Adj(v int) (vertices []int) {
 	return vertices
 }
 
+// Reverse returns the reverse of the digraph.
 func (dg *Digraph) Reverse() *Digraph {
 	reverse := NewDigraph(dg.v)
 	for v := 0; v < dg.v; v++ {
@@ -110,7 +147,7 @@ func (dg *Digraph) Reverse() *Digraph {
 	return reverse
 }
 
-// String returns a string representation of this graph.
+// String returns a string representation of this digraph.
 func (dg *Digraph) String() string {
 	var s strings.Builder
 	fmt.Fprintf(&s, "%d vertices, %d edges \n", dg.v, dg.e)
---
+++ digraph/digraph_test.go
@@ -0,0 +1,96 @@
+package digraph
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/handane123/algorithms/stdin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDigraph_NewDigraph(t *testing.T) {
+	assert := assert.New(t)
+	assert.Panics(func() { NewDigraph(-1) })
+}
+
+func TestDigraph_NewDigraphIn(t *testing.T) {
+	assert := assert.New(t)
+	buf := strings.NewReader("2\n" + "1\n" + "0 1\n")
+	s := bufio.NewScanner(buf)
+	s.Split(bufio.ScanWords)
+	in := &stdin.In{Scanner: s}
+	g, err := NewDigraphIn(in)
+	assert.Nil(err)
+
+	assert.Equal(1, g.E())
+	assert.Equal(2, g.V())
+
+	g1, err1 := NewDigraphIn(nil)
+	assert.Nil(g1)
+	assert.Error(err1)
+
+	buf1 := strings.NewReader("-1\n" + "1\n" + "0 1\n")
+	s1 := bufio.NewScanner(buf1)
+	s1.Split(bufio.ScanWords)
+	in1 := &stdin.In{Scanner: s1}
+	g2, err2 := NewDigraphIn(in1)
+	assert.Nil(g2)
+	assert.Error(err2)
+
+	buf2 := strings.NewReader("2\n" + "-2\n" + "0 1\n")
+	s2 := bufio.NewScanner(buf2)
+	s2.Split(bufio.ScanWords)
+	in2 := &stdin.In{Scanner: s2}
+	g3, err3 := NewDigraphIn(in2)
+	assert.Nil(g3)
+	assert.Error(err3)
+}
+
+func TestDigraph_OutDegree(t *testing.T) {
+	assert := assert.New(t)
+	g := NewDigraph(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+
+	assert.Equal(2, g.OutDegree(0))
+	assert.Panics(func() { g.OutDegree(3) })
+}
+
+func TestDigraph_InDegree(t *testing.T) {
+	assert := assert.New(t)
+	g := NewDigraph(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+
+	assert.Equal(1, g.InDegree(1))
+	assert.Equal(1, g.InDegree(2))
+}
+
+func TestDigraph_Reverse(t *testing.T) {
+	assert := assert.New(t)
+	g := NewDigraph(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+
+	reverse := g.Reverse()
+	assert.Equal(2, reverse.InDegree(0))
+	assert.Equal(1, reverse.OutDegree(1))
+	assert.Equal(1, reverse.OutDegree(2))
+}
+
+func TestDigraph_Adj(t *testing.T) {
+	assert := assert.New(t)
+	g := NewDigraph(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	assert.Equal([]int{2, 1}, g.Adj(0))
+}
+
+func TestDigraph_String(t *testing.T) {
+	assert := assert.New(t)
+	g := NewDigraph(3)
+	g.AddEdge(0, 1)
+	g.AddEdge(0, 2)
+	assert.Equal("3 vertices, 2 edges \n"+"0:  2 1\n"+"1: \n"+"2: \n", g.String())
+}
