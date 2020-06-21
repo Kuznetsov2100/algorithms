072ec716d4cc8e7a9e48f323e7d5abb0a4968065
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-21 11:49:04 +0800

    add directedEulerianPath.go

---
+++ digraph/directedEulerianPath.go
@@ -0,0 +1,89 @@
+package digraph
+
+import (
+	"github.com/emirpasic/gods/lists/arraylist"
+	"github.com/handane123/algorithms/dataStructure/stack/arraystack"
+)
+
+// DirectedEulerianPath struct represents a data type for finding an Eulerian path in a digraph.
+// An Eulerian path is a path (not necessarily simple) that uses every edge in the digraph exactly once.
+// This implementation uses a nonrecursive depth-first search. The constructor take O(E + V) time
+// in the worst case, where E is the number of edges and V is the number of vertices. It uses O(V) extra
+// space (not including the digraph).
+type DirectedEulerianPath struct {
+	path *arraystack.Stack
+}
+
+// NewDirectedEulerianPath computes an Eulerian path in the specified digraph, if one exists.
+func NewDirectedEulerianPath(G *Digraph) *DirectedEulerianPath {
+	dep := &DirectedEulerianPath{}
+
+	// find vertex from which to start potential Eulerian path:
+	// a vertex v with outdegree(v) > indegree(v) if it exits;
+	// otherwise a vertex with outdegree(v) > 0
+	deficit := 0
+	s := nonIsolatedVertex(G)
+	for v := 0; v < G.V(); v++ {
+		if G.OutDegree(v) > G.InDegree(v) {
+			deficit += G.OutDegree(v) - G.InDegree(v)
+			s = v
+		}
+	}
+
+	// digraph can't have an Eulerian path
+	// (this condition is needed)
+	if deficit > 1 {
+		return dep
+	}
+
+	// special case for digraph with zero edges (has a degenerate Eulerian path)
+	if s == -1 {
+		s = 0
+	}
+
+	// create local view of adjacency lists, to iterate one vertex at a time
+	adj := make([]arraylist.Iterator, G.V())
+	for v := 0; v < G.V(); v++ {
+		vertices := arraylist.New()
+		for _, w := range G.Adj(v) {
+			vertices.Add(w)
+		}
+		adj[v] = vertices.Iterator()
+	}
+
+	stack := arraystack.New()
+	stack.Push(s)
+
+	// greedily add to putative cycle, depth-first search style
+	dep.path = arraystack.New()
+	for !stack.IsEmpty() {
+		val, _ := stack.Pop()
+		v := val.(int)
+		for adj[v].Next() {
+			stack.Push(v)
+			v = adj[v].Value().(int)
+		}
+		// push vertex with no more available edges to path
+		dep.path.Push(v)
+	}
+
+	// check if all edges have been used
+	if dep.path.Size() != G.E()+1 {
+		dep.path = nil
+	}
+
+	return dep
+}
+
+// Path returns the sequence of vertices on an Eulerian path.
+func (dep *DirectedEulerianPath) Path() (p []int) {
+	for _, val := range dep.path.Values() {
+		p = append(p, val.(int))
+	}
+	return p
+}
+
+// HasEulerianPath returns true if the digraph has an Eulerian path.
+func (dep *DirectedEulerianPath) HasEulerianPath() bool {
+	return dep.path != nil
+}
---
+++ digraph/directedEulerianPath_test.go
@@ -0,0 +1,60 @@
+package digraph
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+
+	"github.com/handane123/algorithms/stdin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDirectedEulerianPath(t *testing.T) {
+	assert := assert.New(t)
+
+	graphdata1 := "3\n" +
+		"3\n" +
+		"0 1\n" +
+		"1 2\n" +
+		"2 0\n"
+	buf1 := strings.NewReader(graphdata1)
+	scanner1 := bufio.NewScanner(buf1)
+	scanner1.Split(bufio.ScanWords)
+	in1 := &stdin.In{Scanner: scanner1}
+	g1, err1 := NewDigraphIn(in1)
+	assert.Nil(err1)
+	ec1 := NewDirectedEulerianPath(g1)
+	assert.True(ec1.HasEulerianPath())
+	assert.Equal([]int{0, 1, 2, 0}, ec1.Path())
+
+	// digraph with zero edges has a degenerate Eulerian path []int{0}
+	graphdata3 := "1\n" +
+		"0\n"
+	buf3 := strings.NewReader(graphdata3)
+	scanner3 := bufio.NewScanner(buf3)
+	scanner3.Split(bufio.ScanWords)
+	in3 := &stdin.In{Scanner: scanner3}
+	g3, err3 := NewDigraphIn(in3)
+	assert.Nil(err3)
+	ec3 := NewDirectedEulerianPath(g3)
+	assert.True(ec3.HasEulerianPath())
+	assert.Equal([]int{0}, ec3.Path())
+
+	// deficit > 1
+	graphdata4 := "5\n" +
+		"6\n" +
+		"1 2\n" +
+		"1 0\n" +
+		"2 0\n" +
+		"0 3\n" +
+		"1 3\n" +
+		"3 4\n"
+	buf4 := strings.NewReader(graphdata4)
+	scanner4 := bufio.NewScanner(buf4)
+	scanner4.Split(bufio.ScanWords)
+	in4 := &stdin.In{Scanner: scanner4}
+	g4, err4 := NewDigraphIn(in4)
+	assert.Nil(err4)
+	ec4 := NewDirectedEulerianPath(g4)
+	assert.False(ec4.HasEulerianPath())
+}
