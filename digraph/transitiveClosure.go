cc08358c1afcd44f89b33e9384a5793fcb47e41c
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-22 16:36:10 +0800

    add comments

--- digraph/transitiveClosure.go
+++ digraph/transitiveClosure.go
@@ -2,10 +2,16 @@ package digraph
 
 import "fmt"
 
+// TransitiveClosure struct represents a data type for computing the transitive closure of a digraph.
+// This implementation runs depth-first search from each vertex.
+// The constructor takes O(V(V + E)) in the worst case, where V is the number of vertices
+// and E is the number of edges. Each instance method takes O(1) time. It uses O(V2) extra
+// space (not including the digraph).
 type TransitiveClosure struct {
 	tc []*DirectedDFS
 }
 
+// NewTransitiveClosure computes the transitive closure of the digraph G.
 func NewTransitiveClosure(G *Digraph) *TransitiveClosure {
 	tc := make([]*DirectedDFS, G.V())
 	for v := 0; v < G.V(); v++ {
@@ -14,6 +20,7 @@ func NewTransitiveClosure(G *Digraph) *TransitiveClosure {
 	return &TransitiveClosure{tc: tc}
 }
 
+// Reachable returns true if there is a directed path from vertex v to vertex w in the digraph
 func (t *TransitiveClosure) Reachable(v, w int) bool {
 	t.validateVertex(v)
 	t.validateVertex(w)
