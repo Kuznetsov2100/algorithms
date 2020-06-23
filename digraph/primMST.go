86e0ad076dc38baf37dc9ddc9bce998392a9f3ed
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-23 17:43:14 +0800

    add comments

--- digraph/primMST.go
+++ digraph/primMST.go
@@ -6,26 +6,23 @@ import (
 	"github.com/handane123/algorithms/dataStructure/priorityqueue"
 )
 
-type double float64
-
-func (d double) CompareTo(k priorityqueue.Key) int {
-	that := k.(double)
-	if d < that {
-		return -1
-	} else if d > that {
-		return 1
-	} else {
-		return 0
-	}
-}
-
+// PrimMST struct represents a data type for computing a minimum spanning tree in an edge-weighted graph.
+// The edge weights can be positive, zero, or negative and need not be distinct.
+// If the graph is not connected, it computes a minimum spanning forest,
+// which is the union of minimum spanning trees in each connected component.
+// The weight() method returns the weight of a minimum spanning tree and the edges() method returns its edges.
+// This implementation uses Prim's algorithm with an indexed binary heap.
+// The constructor takes O(E log V) time in the worst case, where V is the number of vertices
+// and E is the number of edges. Each instance method takes O(1) time. It uses O(V) extra space
+// (not including the edge-weighted graph).
 type PrimMST struct {
-	edgeTo []*Edge
-	distTo []double
-	marked []bool
+	edgeTo []*Edge  // edgeTo[v] = shortest edge from tree vertex to non-tree vertex
+	distTo []double // distTo[v] = weight of shortest such edge
+	marked []bool   // marked[v] = true if v on tree, false otherwise
 	pq     *priorityqueue.IndexMinPQ
 }
 
+// NewPrimMST compute a minimum spanning tree (or forest) of an edge-weighted graph.
 func NewPrimMST(G *EdgeWeightedGraph) *PrimMST {
 	mst := &PrimMST{
 		edgeTo: make([]*Edge, G.V()),
@@ -45,6 +42,7 @@ func NewPrimMST(G *EdgeWeightedGraph) *PrimMST {
 	return mst
 }
 
+// run Prim's algorithm in graph G, starting from vertex s
 func (mst *PrimMST) prim(G *EdgeWeightedGraph, s int) {
 	mst.distTo[s] = 0.0
 	//nolint:errcheck
@@ -55,12 +53,13 @@ func (mst *PrimMST) prim(G *EdgeWeightedGraph, s int) {
 	}
 }
 
+// scan vertex v
 func (mst *PrimMST) scan(G *EdgeWeightedGraph, v int) {
 	mst.marked[v] = true
 	for _, e := range G.Adj(v) {
 		w := e.Other(v)
 		if mst.marked[w] {
-			continue
+			continue // v-w is obsolete edge
 		}
 		if e.Weight() < float64(mst.distTo[w]) {
 			mst.distTo[w] = double(e.Weight())
@@ -76,6 +75,7 @@ func (mst *PrimMST) scan(G *EdgeWeightedGraph, v int) {
 	}
 }
 
+// Edges returns the edges in a minimum spanning tree (or forest).
 func (mst *PrimMST) Edges() (edges []*Edge) {
 	for v := 0; v < len(mst.edgeTo); v++ {
 		if e := mst.edgeTo[v]; e != nil {
@@ -85,10 +85,24 @@ func (mst *PrimMST) Edges() (edges []*Edge) {
 	return edges
 }
 
+// Weight returns the sum of the edge weights in a minimum spanning tree (or forest).
 func (mst *PrimMST) Weight() float64 {
 	weight := 0.0
 	for _, e := range mst.Edges() {
 		weight += e.Weight()
 	}
 	return weight
+}
+
+type double float64
+
+func (d double) CompareTo(k priorityqueue.Key) int {
+	that := k.(double)
+	if d < that {
+		return -1
+	} else if d > that {
+		return 1
+	} else {
+		return 0
+	}
 }
