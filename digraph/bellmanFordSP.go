b7b5b396c7a7ba6344d905445c515293015f567e
handane123 <7983700+handane123@users.noreply.github.com>
2020-06-27 14:19:02 +0800

    add comments

--- digraph/bellmanFordSP.go
+++ digraph/bellmanFordSP.go
@@ -8,6 +8,14 @@ import (
 	"github.com/handane123/algorithms/dataStructure/stack/arraystack"
 )
 
+// BellmanFordSP struct represents a data type for solving the single-source shortest paths problem
+// in edge-weighted digraphs with no negative cycles. The edge weights can be positive, negative, or zero.
+// This class finds either a shortest path from the source vertex s to every other vertex or
+// a negative cycle reachable from the source vertex.
+// This implementation uses a queue-based implementation of the Bellman-Ford-Moore algorithm.
+// The constructor takes O(E V) time in the worst case, where V is the number of vertices and E
+// is the number of edges. In practice, it performs much better. Each instance method takes O(1) time.
+// It uses O(V) extra space (not including the edge-weighted digraph).
 type BellmanFordSP struct {
 	distTo  []float64
 	edgeTo  []*DirectedEdge
@@ -17,6 +25,7 @@ type BellmanFordSP struct {
 	cycle   []*DirectedEdge
 }
 
+// NewBellmanFordSP computes a shortest paths tree from s to every other vertex in the edge-weighted digraph G.
 func NewBellmanFordSP(G *EdgeWeightedDigraph, s int) *BellmanFordSP {
 	sp := &BellmanFordSP{
 		distTo:  make([]float64, G.V()),
@@ -52,7 +61,7 @@ func (sp *BellmanFordSP) relax(G *EdgeWeightedDigraph, v int) {
 			}
 		}
 		if sp.cost++; sp.cost%G.V() == 0 {
-			sp.FindNegativeCycle()
+			sp.findNegativeCycle()
 			if sp.HasNegativeCycle() {
 				return
 			}
@@ -60,22 +69,12 @@ func (sp *BellmanFordSP) relax(G *EdgeWeightedDigraph, v int) {
 	}
 }
 
+// HasNegativeCycle returns true if there is a negative cycle reachable from the source vertex s
 func (sp *BellmanFordSP) HasNegativeCycle() bool {
 	return sp.cycle != nil
 }
 
-func (sp *BellmanFordSP) FindNegativeCycle() {
-	V := len(sp.edgeTo)
-	spt := NewEdgeWeightedDigraphV(V)
-	for v := 0; v < V; v++ {
-		if sp.edgeTo[v] != nil {
-			spt.AddEdge(sp.edgeTo[v])
-		}
-	}
-	finder := NewEdgeWeightedDirectedCycle(spt)
-	sp.cycle = finder.GetCycle()
-}
-
+// NegativeCycle returns a negative cycle reachable from the source vertex s, or nil if there is no such cycle.
 func (sp *BellmanFordSP) NegativeCycle() (edges []*DirectedEdge) {
 	if !sp.HasNegativeCycle() {
 		return nil
@@ -85,16 +84,19 @@ func (sp *BellmanFordSP) NegativeCycle() (edges []*DirectedEdge) {
 	return edges
 }
 
+// DistTo returns the length of a shortest path from the source vertex s to vertex v.
 func (sp *BellmanFordSP) DistTo(v int) float64 {
 	sp.validateVertex(v)
 	return sp.distTo[v]
 }
 
+// HasPathTo returns true if there is a path from the source s to vertex v
 func (sp *BellmanFordSP) HasPathTo(v int) bool {
 	sp.validateVertex(v)
 	return sp.distTo[v] < math.Inf(1)
 }
 
+// PathTo returns a shortest path from the source s to vertex v.
 func (sp *BellmanFordSP) PathTo(v int) (edges []*DirectedEdge) {
 	sp.validateVertex(v)
 	if sp.HasNegativeCycle() {
@@ -113,6 +115,18 @@ func (sp *BellmanFordSP) PathTo(v int) (edges []*DirectedEdge) {
 		edges = append(edges, e.(*DirectedEdge))
 	}
 	return edges
+}
+
+func (sp *BellmanFordSP) findNegativeCycle() {
+	V := len(sp.edgeTo)
+	spt := NewEdgeWeightedDigraphV(V)
+	for v := 0; v < V; v++ {
+		if sp.edgeTo[v] != nil {
+			spt.AddEdge(sp.edgeTo[v])
+		}
+	}
+	finder := NewEdgeWeightedDirectedCycle(spt)
+	sp.cycle = finder.GetCycle()
 }
 
 func (sp *BellmanFordSP) validateVertex(v int) {
