4020c6a8e4710184fd454c27723d153dc9ca1bca
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-03 20:05:25 +0800

    add comments

--- str/tst.go
+++ str/tst.go
@@ -2,6 +2,16 @@ package str
 
 import "strings"
 
+// TST stuct represents an symbol table of key-value pairs, with string keys and generic values.
+// It supports the usual put, get, contains, delete, size, and is-empty methods. It also provides
+// character-based methods for finding the string in the symbol table that is the longest prefix
+// of a given prefix, finding all strings in the symbol table that start with a given prefix,
+// and finding all strings in the symbol table that match a given pattern. A symbol table implements
+// the associative array abstraction: when associating a value with a key that is already in the symbol
+// table, the convention is to replace the old value with the new value. Unlike Map, this struct uses
+// the convention that values cannot be nil—setting the value associated with a key to nil
+// is equivalent to deleting the key from the symbol table.
+// This implementation uses a ternary search trie.
 type TST struct {
 	n    int
 	root *nodeC
@@ -15,23 +25,28 @@ type nodeC struct {
 	val   interface{}
 }
 
+// NewTST initializes an empty string symbol table.
 func NewTST() *TST {
 	return &TST{}
 }
 
+// Size returns the number of key-value pairs in this symbol table.
 func (st *TST) Size() int {
 	return st.n
 }
 
+// IsEmpty returns true if the symbol table is empty.
 func (st *TST) IsEmpty() bool {
 	return st.n == 0
 }
 
+// Contains returns true if the symbol table contains the given key.
 func (st *TST) Contains(key string) bool {
 
 	return st.Get(key) != nil
 }
 
+// Get returns the value associated with the given key.
 func (st *TST) Get(key string) interface{} {
 	x := st.get(st.root, key, 0)
 	if x == nil {
@@ -58,6 +73,8 @@ func (st *TST) get(x *nodeC, key string, d int) *nodeC {
 	}
 }
 
+// Put inserts the key-value pair into the symbol table,
+// overwriting the old value with the new value if the key is already in the symbol table.
 func (st *TST) Put(key string, val interface{}) {
 	if !st.Contains(key) {
 		st.n++
@@ -84,6 +101,8 @@ func (st *TST) put(x *nodeC, key string, val interface{}, d int) *nodeC {
 	return x
 }
 
+// LongestPrefixOf returns the string in the symbol table that
+// is the longest prefix of query, or nil, if no such string.
 func (st *TST) LongestPrefixOf(query string) string {
 	if query == "" {
 		return ""
@@ -106,12 +125,14 @@ func (st *TST) LongestPrefixOf(query string) string {
 	return query[:length]
 }
 
+// Keys returns all keys in the symbol table as a slice
 func (st *TST) Keys() (keys []string) {
 	var s strings.Builder
 	st.collectPrefix(st.root, &s, &keys)
 	return keys
 }
 
+// KeysWithPrefix returns all of the keys in the set that start with prefix.
 func (st *TST) KeysWithPrefix(prefix string) (results []string) {
 	x := st.get(st.root, prefix, 0)
 	if x == nil {
@@ -142,6 +163,8 @@ func (st *TST) collectPrefix(x *nodeC, prefix *strings.Builder, results *[]strin
 	st.collectPrefix(x.right, prefix, results)
 }
 
+// KeysThatMatch returns all of the keys in the symbol table that match pattern,
+// where "." symbol is treated as a wildcard character.
 func (st *TST) KeysThatMatch(pattern string) (results []string) {
 	var s strings.Builder
 	st.collectMatch(st.root, &s, 0, pattern, &results)
