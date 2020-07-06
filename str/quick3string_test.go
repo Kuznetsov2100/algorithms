ccf70d09cd8617e357042f744c7428d227cd1698
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-06 19:50:24 +0800

    add more test case

--- str/quick3string_test.go
+++ str/quick3string_test.go
@@ -28,6 +28,28 @@ func TestQuick3string(t *testing.T) {
 		"was", "wee", "yes", "yet", "zoo",
 	}
 
+	original1 := []string{
+		"bed", "bug", "dad", "yes", "zoo",
+		"now", "for", "tip", "ilk", "dim",
+		"tag", "jot", "sob", "nob", "sky",
+		"hut", "men", "egg", "jam", "jay",
+		"owl", "joy", "rap", "gig", "wee",
+		"was", "wad", "fee", "tap", "tar",
+		"dug", "few", "all", "bad", "yet",
+	}
+	expected1 := []string{
+		"all", "bad", "bed", "bug", "dad",
+		"dim", "dug", "egg", "fee", "few",
+		"for", "gig", "hut", "ilk", "jam",
+		"jay", "jot", "joy", "men", "nob",
+		"now", "owl", "rap", "sky", "sob",
+		"tag", "tap", "tar", "tip", "wad",
+		"was", "wee", "yes", "yet", "zoo",
+	}
+
 	Quick3string(original)
 	assert.Equal(expected, original)
+
+	Quick3string(original1)
+	assert.Equal(expected1, original1)
 }
