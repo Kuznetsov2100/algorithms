dc7ff4754abfe4c5f3c0c5a22d202b9e831123fd
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-07 22:46:05 +0800

    add comments

--- str/nfa.go
+++ str/nfa.go
@@ -7,12 +7,16 @@ import (
 	"github.com/handane123/algorithms/digraph"
 )
 
+// NFA struct provides a data type for creating a nondeterministic finite state automaton (NFA) from
+// a regular expression and testing whether a given string is matched by that regular expression.
+// It supports the following operations: concatenation, closure, binary or, and parentheses.
 type NFA struct {
-	graph  *digraph.Digraph
-	regexp string
-	m      int
+	graph  *digraph.Digraph // digraph of  ε-transitions
+	regexp string           // regular expression
+	m      int              // number of characters in regular expression
 }
 
+// NewNFA initializes the NFA from the specified regular expression.
 func NewNFA(regexp string) *NFA {
 	nfa := &NFA{regexp: regexp, m: len(regexp), graph: digraph.NewDigraph(len(regexp) + 1)}
 	ops := arraystack.New()
@@ -48,43 +52,49 @@ func NewNFA(regexp string) *NFA {
 	return nfa
 }
 
+// Recognizes returns true if the text is matched by the regular expression.
 func (nfa *NFA) Recognizes(txt string) bool {
 	dfs := digraph.NewDirectedDFS(nfa.graph, 0)
 	var pc []int
+	//states reachable from start by ε-transitions
 	for v := 0; v < nfa.graph.V(); v++ {
 		if dfs.IsMarked(v) {
 			pc = append(pc, v)
 		}
 	}
 
-	for i := 0; i < len(txt); i++ {
+	// Compute possible NFA states for txt[i+1]
+	for i := range txt {
 		if string(txt[i]) == "*" || string(txt[i]) == "|" || string(txt[i]) == "(" || string(txt[i]) == ")" {
 			panic(fmt.Sprintf("text contains the metacharacter '%c'\n", txt[i]))
 		}
-		match := make([]int, 0)
+		states := make([]int, 0) // set of states reachable after scanning past txt[i]
 		for _, v := range pc {
 			if v == nfa.m {
-				continue
+				continue // not necessarily a match (RE needs to match full text)
 			}
 			if nfa.regexp[v] == txt[i] || string(nfa.regexp[v]) == "." {
-				match = append(match, v+1)
+				states = append(states, v+1)
 			}
 		}
-		dfs = digraph.NewDirectedDFSources(nfa.graph, match)
+		// follow ε-transitions
+		dfs = digraph.NewDirectedDFSources(nfa.graph, states)
 		pc = make([]int, 0)
 		for v := 0; v < nfa.graph.V(); v++ {
 			if dfs.IsMarked(v) {
 				pc = append(pc, v)
 			}
 		}
-		if len(pc) == 0 {
+
+		if len(pc) == 0 { // no states reachable
 			return false
 		}
 	}
 
+	// check for accept state
 	for _, v := range pc {
 		if v == nfa.m {
-			return true
+			return true // accept if can end in state M
 		}
 	}
 	return false
