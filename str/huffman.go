efb026e0332db5fe7097606a4a041198526fc5b5
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-11 20:19:34 +0800

    add comments

--- str/huffman.go
+++ str/huffman.go
@@ -11,40 +11,47 @@ import (
 // Huffman struct provides static methods for compressing and expanding a binary input
 // using Huffman codes over the 8-bit extended ASCII alphabet.
 type Huffman struct {
-	R   int
-	in  *binaryin.BinaryIn
-	out *binaryout.BinaryOut
+	R   int                  // alphabet size of extended ASCII
+	in  *binaryin.BinaryIn   // input stream
+	out *binaryout.BinaryOut // output stream
 }
 
 func NewHuffman(r io.Reader, w io.Writer) *Huffman {
-
 	return &Huffman{R: 256, in: binaryin.NewBinaryIn(r), out: binaryout.NewBinaryOut(w)}
-
 }
 
-// Compress reads a sequence of 8-bit bytes from standard input;
-// compresses them using Huffman codes with an 8-bit alphabet;
-// and writes the results to standard output.
+// Compress reads a sequence of 8-bit bytes from input stream;compresses them using Huffman codes
+// with an 8-bit alphabet; and writes the results to ouput stream.
 func (hf *Huffman) Compress() {
-	s, err := hf.in.ReadString()
-	if err != nil {
-		panic(err)
-	}
-	freq := make([]int, hf.R)
-	for i := range s {
-		freq[s[i]]++
-	}
-	root := hf.buildTrie(freq)
-	st := make([]string, hf.R)
-	hf.buildCode(st, root, "")
-	hf.writeTrie(root)
-	err = hf.out.WriteInt(len(s))
+	// read the input
+	input, err := hf.in.ReadString()
 	if err != nil {
 		panic(err)
 	}
 
-	for i := range s {
-		code := st[s[i]]
+	// tabulate frequency counts
+	freq := make([]int, hf.R)
+	for i := range input {
+		freq[input[i]]++
+	}
+
+	// build Huffman trie
+	root := hf.buildTrie(freq)
+	st := make([]string, hf.R)
+
+	// build code table
+	hf.buildCode(st, root, "")
+	// print trie for decoder
+	hf.writeTrie(root)
+
+	// print number of bytes in original uncompressed message
+	if err := hf.out.WriteInt(len(input)); err != nil {
+		panic(err)
+	}
+
+	// use Huffman code to encode input
+	for i := range input {
+		code := st[input[i]]
 		for j := range code {
 			if code[j] == '0' {
 				hf.out.WriteBit(false)
@@ -55,15 +62,21 @@ func (hf *Huffman) Compress() {
 			}
 		}
 	}
+	// close output stream
 	hf.out.Close()
 }
 
+// Expand reads a sequence of bits that represents a Huffman-compressed message from input stream;
+// expands them; and writes the results to output stream.
 func (hf *Huffman) Expand() {
+	// read in Huffman trie from input stream
 	root := hf.readTrie()
+	// number of bytes to write
 	length, err := hf.in.ReadInt()
 	if err != nil {
 		panic(err)
 	}
+	// decode using the Huffman trie
 	for i := 0; i < length; i++ {
 		x := root
 		for !x.isLeaf() {
@@ -77,13 +90,49 @@ func (hf *Huffman) Expand() {
 				x = x.left
 			}
 		}
-		err := hf.out.WriteBitR(int(x.ch), 8)
+		err := hf.out.WriteByte(x.ch)
 		if err != nil {
 			panic(err)
 		}
 
 	}
 	hf.out.Close()
+}
+
+// build the Huffman trie given frequencies
+func (hf *Huffman) buildTrie(freq []int) *hnode {
+	// initialze priority queue with singleton trees
+	pq := priorityqueue.NewMinPQ()
+	for b := 0; b < hf.R; b++ {
+		if freq[b] > 0 {
+			pq.Insert(newhnode(byte(b), freq[b], nil, nil))
+		}
+	}
+
+	// merge two smallest trees
+	// Nodes with low frequencies end up far down in the trie,
+	// and nodes with high frequencies end up near the root of the trie.
+	// The frequency in the root equals the number of characters in the input
+	for pq.Size() > 1 {
+		kl, _ := pq.DelMin()
+		left := kl.(*hnode)
+		kr, _ := pq.DelMin()
+		right := kr.(*hnode)
+		parent := newhnode('0', left.freq+right.freq, left, right)
+		pq.Insert(parent)
+	}
+	tree, _ := pq.DelMin()
+	return tree.(*hnode)
+}
+
+// make a lookup table from symbols and their encodings
+func (hf *Huffman) buildCode(st []string, x *hnode, s string) {
+	if !x.isLeaf() {
+		hf.buildCode(st, x.left, s+"0")
+		hf.buildCode(st, x.right, s+"1")
+	} else {
+		st[x.ch] = s
+	}
 }
 
 func (hf *Huffman) readTrie() *hnode {
@@ -102,39 +151,11 @@ func (hf *Huffman) readTrie() *hnode {
 	}
 }
 
-func (hf *Huffman) buildTrie(freq []int) *hnode {
-	pq := priorityqueue.NewMinPQ()
-	for b := 0; b < hf.R; b++ {
-		if freq[b] > 0 {
-			pq.Insert(newhnode(byte(b), freq[b], nil, nil))
-		}
-	}
-
-	for pq.Size() > 1 {
-		kl, _ := pq.DelMin()
-		left := kl.(*hnode)
-		kr, _ := pq.DelMin()
-		right := kr.(*hnode)
-		parent := newhnode(0, left.freq+right.freq, left, right)
-		pq.Insert(parent)
-	}
-	k, _ := pq.DelMin()
-	return k.(*hnode)
-}
-
-func (hf *Huffman) buildCode(st []string, x *hnode, s string) {
-	if !x.isLeaf() {
-		hf.buildCode(st, x.left, s+"0")
-		hf.buildCode(st, x.right, s+"1")
-	} else {
-		st[x.ch] = s
-	}
-}
-
+// write bitstring-encoded trie to output stream
 func (hf *Huffman) writeTrie(x *hnode) {
 	if x.isLeaf() {
 		hf.out.WriteBit(true)
-		err := hf.out.WriteBitR(int(x.ch), 8)
+		err := hf.out.WriteByte(x.ch)
 		if err != nil {
 			panic(err)
 		}
@@ -145,6 +166,7 @@ func (hf *Huffman) writeTrie(x *hnode) {
 	hf.writeTrie(x.right)
 }
 
+// // Huffman trie node
 type hnode struct {
 	ch    byte
 	freq  int
@@ -152,14 +174,16 @@ type hnode struct {
 	right *hnode
 }
 
-func newhnode(ch byte, freq int, left *hnode, right *hnode) *hnode {
+func newhnode(ch byte, freq int, left, right *hnode) *hnode {
 	return &hnode{ch: ch, freq: freq, left: left, right: right}
 }
 
+// // is the node a leaf node?
 func (node *hnode) isLeaf() bool {
 	return node.left == nil && node.right == nil
 }
 
+// // compare, based on frequency
 func (node *hnode) CompareTo(key priorityqueue.Key) int {
 	that := key.(*hnode)
 	if node.freq < that.freq {
--- str/huffman_test.go
+++ str/huffman_test.go
@@ -13,8 +13,9 @@ func TestHuffmanCompress(t *testing.T) {
 	buf := bytes.NewBufferString("ABRACADABRA!")
 	huffman := NewHuffman(buf, &b)
 	huffman.Compress()
-	assert.Equal("PJ\"CCT\xa8@\x00\x00\x01\x8f\x96\x8f\x94", b.String())
-
+	assert.Equal(
+		[]byte{0x50, 0x4a, 0x22, 0x43, 0x43, 0x54, 0xa8, 0x40, 0x00, 0x00, 0x01, 0x8f, 0x96, 0x8f, 0x94},
+		b.Bytes())
 	var b1 bytes.Buffer
 	huffman1 := NewHuffman(bytes.NewBuffer(nil), &b1)
 	assert.Panics(func() { huffman1.Compress() })
@@ -24,7 +25,7 @@ func TestHuffmanCompress(t *testing.T) {
 func TestHuffmanExpand(t *testing.T) {
 	assert := assert.New(t)
 	var b bytes.Buffer
-	buf := bytes.NewBufferString("PJ\"CCT\xa8@\x00\x00\x01\x8f\x96\x8f\x94")
+	buf := bytes.NewBuffer([]byte{0x50, 0x4a, 0x22, 0x43, 0x43, 0x54, 0xa8, 0x40, 0x00, 0x00, 0x01, 0x8f, 0x96, 0x8f, 0x94})
 	huffman := NewHuffman(buf, &b)
 	huffman.Expand()
 	assert.Equal("ABRACADABRA!", b.String())
