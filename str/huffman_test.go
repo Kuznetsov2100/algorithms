c2bee9a2489454f10cac37a6d55e067b35b3e620
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-11 22:28:45 +0800

    huffman.go with test coverage 99.1%

--- str/huffman_test.go
+++ str/huffman_test.go
@@ -29,4 +29,22 @@ func TestHuffmanExpand(t *testing.T) {
 	huffman := NewHuffman(buf, &b)
 	huffman.Expand()
 	assert.Equal("ABRACADABRA!", b.String())
+
+	buf1 := bytes.NewBuffer([]byte{0x50})
+	var b1 bytes.Buffer
+	huffman1 := NewHuffman(buf1, &b1)
+	assert.Panics(func() { huffman1.readTrie() })
+
+	var b2 bytes.Buffer
+	huffman2 := NewHuffman(bytes.NewBuffer(nil), &b2)
+	assert.Panics(func() { huffman2.readTrie() })
+
+	var b3 bytes.Buffer
+	huffman3 := NewHuffman(bytes.NewBuffer([]byte{0x50, 0x4a, 0x22, 0x43, 0x43, 0x54, 0xa8, 0x40}), &b3)
+	assert.Panics(func() { huffman3.Expand() })
+
+	var b4 bytes.Buffer
+	huffman4 := NewHuffman(bytes.NewBuffer([]byte{0x50, 0x4a, 0x22, 0x43, 0x43, 0x54, 0xa8, 0x40, 0x00, 0x00, 0x01, 0x8f}), &b4)
+	assert.Panics(func() { huffman4.Expand() })
+
 }
