eda2f8c96692028eb1af253dbc946758bb206762
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-13 15:37:51 +0800

    change to unexported fields

--- str/lzw.go
+++ str/lzw.go
@@ -11,16 +11,16 @@ import (
 // LZW struct provides static methods for compressing and expanding a binary input
 // using LZW compression over the 8-bit extended ASCII alphabet with 12-bit codewords.
 type LZW struct {
-	R   int // number of input chars:256
-	L   int // number of codewords = 2^W = 4096
-	W   int // codeword width:12
-	in  *binaryin.BinaryIn
-	out *binaryout.BinaryOut
+	asciiR  int // number of input chars:256
+	numberL int // number of codewords = 2^W = 4096
+	widthW  int // codeword width:12
+	in      *binaryin.BinaryIn
+	out     *binaryout.BinaryOut
 }
 
 // NewLZW constructs the LZW struct
 func NewLZW(r io.Reader, w io.Writer) *LZW {
-	return &LZW{R: 256, L: 4096, W: 12,
+	return &LZW{asciiR: 256, numberL: 4096, widthW: 12,
 		in:  binaryin.NewBinaryIn(r),
 		out: binaryout.NewBinaryOut(w),
 	}
@@ -35,45 +35,45 @@ func (lzw *LZW) Compress() {
 		panic(err)
 	}
 	st := NewTST()
-	for i := 0; i < lzw.R; i++ {
+	for i := 0; i < lzw.asciiR; i++ {
 		st.Put(fmt.Sprintf("%c", i), i)
 	}
-	code := lzw.R + 1 // R is codeword for EOF
+	code := lzw.asciiR + 1 // R is codeword for EOF
 
 	for len(input) > 0 {
 		s := st.LongestPrefixOf(input) // Find max prefix match s.
 		//nolint:errcheck
-		lzw.out.WriteBitR(st.Get(s).(int), lzw.W) // Print s's encoding.
+		lzw.out.WriteBitR(st.Get(s).(int), lzw.widthW) // Print s's encoding.
 		t := len(s)
-		if t < len(input) && code < lzw.L {
+		if t < len(input) && code < lzw.numberL {
 			st.Put(input[:t+1], code) // Add s to symbol table.
 			code++
 		}
 		input = input[t:] // Scan past s in input.
 	}
 	//nolint:errcheck
-	lzw.out.WriteBitR(lzw.R, lzw.W)
+	lzw.out.WriteBitR(lzw.asciiR, lzw.widthW)
 	lzw.out.Close()
 }
 
 // Expand reads a sequence of bit encoded using LZW compression with 12-bit codewords from input stream;
 // expands them; and writes the results to output stream.
 func (lzw *LZW) Expand() {
-	st := make([]string, lzw.L)
+	st := make([]string, lzw.numberL)
 	var i int // next available codeword value
 
 	// initialize symbol table with all 1-character strings
-	for i = 0; i < lzw.R; i++ {
+	for i = 0; i < lzw.asciiR; i++ {
 		st[i] = fmt.Sprintf("%c", i)
 	}
 	st[i] = "" // (unused) lookahead for EOF
 	i++
 
-	codeword, err := lzw.in.ReadIntR(lzw.W)
+	codeword, err := lzw.in.ReadIntR(lzw.widthW)
 	if err != nil {
 		panic(err)
 	}
-	if codeword == lzw.R {
+	if codeword == lzw.asciiR {
 		return // expanded message is empty string
 	}
 
@@ -81,18 +81,18 @@ func (lzw *LZW) Expand() {
 	for {
 		//nolint:errcheck
 		lzw.out.WriteString(val)
-		codeword, err = lzw.in.ReadIntR(lzw.W)
+		codeword, err = lzw.in.ReadIntR(lzw.widthW)
 		if err != nil {
 			panic(err)
 		}
-		if codeword == lzw.R {
+		if codeword == lzw.asciiR {
 			break
 		}
 		s := st[codeword]
 		if i == codeword {
 			s = val + string(val[0]) // special case hack
 		}
-		if i < lzw.L {
+		if i < lzw.numberL {
 			st[i] = val + string(s[0])
 			i++
 		}
