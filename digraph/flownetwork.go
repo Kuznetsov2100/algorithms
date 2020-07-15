32bbf150acae3b65bb340d67c503b08b47feb799
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-15 11:20:30 +0800

    add comments

--- digraph/flownetwork.go
+++ digraph/flownetwork.go
@@ -10,12 +10,19 @@ import (
 	"github.com/handane123/algorithms/io/stdin"
 )
 
+// FlowNetwork struct represents a capacitated network with vertices named 0 through V - 1,
+// where each directed edge is of type FlowEdge and has a real-valued capacity and flow.
+// It supports the following two primary operations: add an edge to the network,
+// iterate over all of the edges incident to or from a vertex.
+// It also provides methods for returning the number of vertices V and the number of edges E.
+// Parallel edges and self-loops are permitted.
 type FlowNetwork struct {
 	v   int
 	e   int
 	adj []*bag.Bag
 }
 
+// NewFlowNetwork initializes an empty flow network with V vertices and 0 edges.
 func NewFlowNetwork(V int) *FlowNetwork {
 	if V < 0 {
 		panic("number of vertices in a graph must be non-negative")
@@ -27,6 +34,7 @@ func NewFlowNetwork(V int) *FlowNetwork {
 	return fn
 }
 
+// NewFlowNetworkVE initializes a random flow network with V vertices and E edges.
 func NewFlowNetworkVE(V, E int) *FlowNetwork {
 	fn := NewFlowNetwork(V)
 	if E < 0 {
@@ -42,6 +50,7 @@ func NewFlowNetworkVE(V, E int) *FlowNetwork {
 	return fn
 }
 
+// NewFlowNetworkIn initializes a flow network from an input stream.
 func NewFlowNetworkIn(in *stdin.In) *FlowNetwork {
 	fn := NewFlowNetwork(in.ReadInt())
 	E := in.ReadInt()
@@ -59,6 +68,7 @@ func NewFlowNetworkIn(in *stdin.In) *FlowNetwork {
 	return fn
 }
 
+// AddEdge adds the edge e to the network.
 func (fn *FlowNetwork) AddEdge(e *FlowEdge) {
 	v := e.From()
 	w := e.To()
@@ -69,14 +79,17 @@ func (fn *FlowNetwork) AddEdge(e *FlowEdge) {
 	fn.e++
 }
 
+// V returns the number of vertices in the edge-weighted graph.
 func (fn *FlowNetwork) V() int {
 	return fn.v
 }
 
+// E returns the number of edges in the edge-weighted graph.
 func (fn *FlowNetwork) E() int {
 	return fn.e
 }
 
+// Adj returns the edges incident on vertex v (includes both edges pointing to and from v).
 func (fn *FlowNetwork) Adj(v int) (edges []*FlowEdge) {
 	fn.validateVertex(v)
 	for _, val := range fn.adj[v].Values() {
@@ -85,6 +98,7 @@ func (fn *FlowNetwork) Adj(v int) (edges []*FlowEdge) {
 	return edges
 }
 
+// Edges return list of all edges - excludes self loops
 func (fn *FlowNetwork) Edges() (edges []*FlowEdge) {
 	for v := 0; v < fn.v; v++ {
 		for _, e := range fn.adj[v].Values() {
@@ -97,6 +111,7 @@ func (fn *FlowNetwork) Edges() (edges []*FlowEdge) {
 	return edges
 }
 
+// String returns a string representation of the flow network.
 func (fn *FlowNetwork) String() string {
 	var s strings.Builder
 	fmt.Fprintf(&s, "%d %d\n", fn.v, fn.e)
