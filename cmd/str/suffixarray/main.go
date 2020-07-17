53f2f4df793ea788386eaa8b5e86bb2ba27a9608
handane123 <7983700+handane123@users.noreply.github.com>
2020-07-17 17:07:45 +0800

    update

--- cmd/str/suffixarray/main.go
+++ cmd/str/suffixarray/main.go
@@ -1,5 +1,23 @@
 package main
 
+//   A data type that computes the suffix array of a string.
+
+//    % java SuffixArray < abra.txt
+//     i ind lcp rnk  select
+//    ---------------------------
+//     0  11   -   0  "!"
+//     1  10   0   1  "A!"
+//     2   7   1   2  "ABRA!"
+//     3   0   4   3  "ABRACADABRA!"
+//     4   3   1   4  "ACADABRA!"
+//     5   5   1   5  "ADABRA!"
+//     6   8   0   6  "BRA!"
+//     7   1   3   7  "BRACADABRA!"
+//     8   4   0   8  "CADABRA!"
+//     9   6   0   9  "DABRA!"
+//    10   9   0  10  "RA!"
+//    11   2   2  11  "RACADABRA!"
+
 import (
 	"fmt"
 	"regexp"
@@ -9,8 +27,8 @@ import (
 )
 
 func main() {
-	space := regexp.MustCompile(`\s+`)
-	s := space.ReplaceAllString(stdin.ReadAll(), " ")
+	whitespace := regexp.MustCompile(`\s+`)
+	s := whitespace.ReplaceAllString(stdin.ReadAll(), " ")
 	suffix := str.NewSuffixArray(s)
 
 	fmt.Println("  i ind lcp rnk select")
